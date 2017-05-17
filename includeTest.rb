843696e15800233723e57caca7b2f73c3dcd3eb2
rajendrarawat17 <rajendra.rawat17@gmail.com>
2017-05-17 16:34:53 +0530

    includeTest.rb
    Include test

---
+++ includeTest.rb
@@ -0,0 +1,13 @@
+module MyModule
+  class Myclass
+    def initialize()
+      puts "This is constructor"
+    end
+    def myMethod
+      puts "I am in myMethod"
+    end
+  end
+end
+
+myObject=MyModule::Myclass.new
+myObject.myMethod()
